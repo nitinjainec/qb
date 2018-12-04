c6330e30e2774201f8b568bda30e7e380d5b1354
Nitin <nitin@localhost.localdomain>
2018-12-04 16:33:39 +0530

    updated test case

--- test/quote.cpp
+++ test/quote.cpp
@@ -81,9 +81,38 @@ void test () {
   //assert (*(reinterpret_cast<const RecordType *>(ch)) == QUOTE);
 }
 
+class MockReader : public IReader {
+  bool _eod;
+public:
+  
+  MockReader () : _eod (false) { }
+  ByteBuffer getData () {
+    _eod = true;
+    return "2013-04-12 17:00:00.006,ZNM3,132.9531,132.9688,697,3493";
+  }
+  bool eod () { return _eod; }
+};
+
+void test2 () {
+  IReaderPtr reader (new MockReader ());
+  IParserPtr parser (new CSVParser (reader));
+
+  assert (!parser->eor ());
+  RecordPtr record_a = parser->nextRecord ();
+  assert (parser->eor ());
+
+  ByteBuffer buffer = record_a->toByteBuffer ();
+
+  assert (RecordFactory::canCreate (buffer));
+  RecordPtr record_b = RecordFactory::create (buffer);
+
+  assert (record_a->toString () == record_b->toString ());
+  std::cout << "Quote test passed";
+}
 
 int main () {
   Logger::create ();
     
-  test ();
+  //test ();
+  test2 ();
 }
